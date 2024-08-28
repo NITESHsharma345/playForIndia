d76a9bf8dd4236113b88fe116947e9f6395e4203
NITESHsharma345 <158508711+NITESHsharma345@users.noreply.github.com>
2024-08-28 23:26:06 +0530

    I am adding javascript_file and rules page

---
+++ script.js
@@ -0,0 +1,40 @@
+const sportSelector = document.getElementById('sportSelector');
+  const rules = document.querySelectorAll('.game_rule ul');
+
+  sportSelector.addEventListener('change', function () {
+    // Hide all the rules
+    rules.forEach(rule => {
+      rule.style.display = 'none';
+    });
+
+    // Show the selected sport's rules
+    const selectedSport = sportSelector.value;
+    if (selectedSport !== 'disable') {
+      const selectedRule = document.getElementById(`${selectedSport}`);
+      if (selectedRule) {
+        selectedRule.style.display = 'block';
+      }
+    }
+  });
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  document.addEventListener("DOMContentLoaded", (event) => {
+
+    const params = new URLSearchParams(window.location.search);
+    const sport = params.get('sport');
+    document.getElementById('sportSelector').value = capitalizeFirstLetter(sport);
+
+    const selectedSport = sportSelector.value;
+    if (selectedSport !== 'disable') {
+      const selectedRule = document.getElementById(`${selectedSport}`);
+      if (selectedRule) {
+        selectedRule.style.display = 'block';
+      }
+    }
+});
+
+
+
